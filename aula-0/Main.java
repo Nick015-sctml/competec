4a885899f19bed80118df928ddbf067e295c7924
Nick015-sctml <nicosms2024@gmail.com>
2025-04-26 11:27:16 -0300

    Dia 1 (26/04)

--- aula-0/Main.java
+++ aula-0/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	    
+	    Scanner entrada = new Scanner(System.in);
+	    
+	    System.out.println("Digite o valor da Laranja");
+	    int Laranja=entrada.nextInt();
+	    
+	    System.out.println("Digite o valor da Uva");
+	    int Uva=entrada.nextInt();
+	    
+	    System.out.println("Seu nome começa com qual letra?");
+	    char nome=entrada.next().charAt(0);
+	    
+	    System.out.println("Quantos anos tem?");
+	    int idade=entrada.nextInt();
+	    
+	    System.out.println("você tomou café hoje?");
+	    boolean cafe=entrada.nextBoolean();
+		System.out.println("O valor final é: R$"+(Laranja+Uva)+",00");
+		System.out.println("Meu nome começa com a letra "+nome);
+		System.out.println("eu tenho "+idade+" anos");
+		System.out.println("eu tomei café da manhã hoje? "+cafe);
 	}
 }
