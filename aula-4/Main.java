2d450b785fd0f3428ab705da3aba6ad527b9255d
Nick015-sctml <nicosms2024@gmail.com>
2025-05-24 11:23:11 -0300

    Aula4_Feita

--- aula-4/Main.java
+++ aula-4/Main.java
@@ -1,7 +1,76 @@
+//Questão 1 Enqunto
+
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
-		System.out.println("Hello World");
+	    Scanner input = new Scanner(System.in);
+    	int x = 0;
+    	
+    	System.out.println("Digite uma palavra: ");
+        String palavra = input.nextLine();
+	
+    	while (x < 5) {
+    	    
+		System.out.println(palavra);
+		x++;
+    	}
+	}
+}
+
+// Questão 2 Soma de 1 até 100
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		int y = 1;
+		int somatotal = 0;
+	
+    	while (y <= 100) {
+    	somatotal += y;
+    	    
+    	System.out.println(somatotal + " ");
+	
+		y++;
+    	}
+	}
+}
+
+//Questão 3 For Tabuada
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    
+    	
+    	System.out.print("Digite um numero: ");
+        int numero = input.nextInt();
+        
+        for (int N = 1; N <=10; N++){
+      
+        System.out.println(N*numero + " ");
+        }
+	}
+	
+	//Questão 4 N Impares
+
+
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    
+    	
+    	System.out.print("Digite um numero: ");
+        int numero = input.nextInt();
+        
+        for (int N = 1; N < numero; N+=2){
+      
+        System.out.println(N + " ");
+        }
 	}
 }
